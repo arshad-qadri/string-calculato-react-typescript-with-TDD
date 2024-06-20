44171bad3de6a339c973e6498de0e8b8ee5b9d0a
Arshad Qadri <arshad.qadri@caprium.com>
2024-06-20 20:12:35 +0530

    single number string and  comma separated string test case added

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,6 +1,13 @@
 import sum from "./sum";
 
-
 test("Test case for empty string", () => {
   expect(sum("")).toBe(0);
-})
+});
+
+test("Test case for single number string", () => {
+  expect(sum("1")).toBe(1);
+});
+
+test("Test case for comma separated string", () => {
+  expect(sum("1,5")).toBe(6);
+});
