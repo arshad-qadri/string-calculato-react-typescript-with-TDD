d473dce3e0a3068740d7474a2cd33eb4a1843214
Arshad Qadri <arshad.qadri@caprium.com>
2024-06-20 20:06:50 +0530

    Revert "Test case for empty string"
    This reverts commit 5f6afef658ec935daa24845cc80cd11bd4c25a9c.

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -4,7 +4,3 @@ import sum from "./sum";
 test("Test case for empty string", () => {
   expect(sum("")).toBe(0);
 })
-
-test("Test case for single number string", () => {
-    expect(sum("1")).toBe(1);
-  });
