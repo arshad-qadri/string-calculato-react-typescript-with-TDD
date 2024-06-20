5f6afef658ec935daa24845cc80cd11bd4c25a9c
Arshad Qadri <arshad.qadri@caprium.com>
2024-06-20 20:01:05 +0530

    test case added for comma seperated numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -4,3 +4,7 @@ import sum from "./sum";
 test("Test case for empty string", () => {
   expect(sum("")).toBe(0);
 })
+
+test("Test case for single number string", () => {
+    expect(sum("1")).toBe(1);
+  });
--- src/sum.ts
+++ src/sum.ts
@@ -2,7 +2,23 @@ const sum = (text: string): number => {
   if (text === "") {
     return 0;
   }
-  return 0;
+  let delimiter = ",";
+  let numbersString = text;
+
+
+  const numbersArray = numbersString.split(delimiter);
+  const result: number = numbersArray.reduce(
+    (accumulator: number, currentValue: string) => {
+      const num = parseInt(currentValue.trim(), 10);
+      if (isNaN(num)) {
+        return accumulator;
+      }  else {
+        return accumulator + num;
+      }
+    },
+    0
+  );
+  return result;
 };
 
 export default sum;
